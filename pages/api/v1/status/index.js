7b52d6f4efda2cc929f65b5430d1f2869041623b
lalakagawa <lalakagawa@hotmail.com>
2024-06-30 10:50:27 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
   response.status(200).json({ chave: "Massinha" });
 }
 
